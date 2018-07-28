63e712a0001610a52b7704ae45160d73b433ba69
tkjone <thomasmattacchione@gmail.com>
2018-07-27 20:04:29 -0400

    Add routes to web server - rest api
    This finalizes the REST version of this revenge for the api portion

--- web-server-api/README.md
+++ web-server-api/README.md
@@ -1,5 +1,57 @@
 # Web Server API
 
-This is the fourth instalment we are building upon the [Web Server Reloaded](https://github.com/tkjone/reveng-edge/tree/master/web-server-reloaded) project.
+This is the fourth instalment we are building upon the [Web Server Reloaded](https://github.com/tkjone/reveng-edge/tree/master/web-server-reloaded) project. This is an example of what it would look like to build web api for an application using the Edge system.
 
-Sorry, I have not been able to work on this yet.
+NOTES:
+
+TODO: go through the express API's and try to see if we can follow one of the good ones here.
+
+TODO: turn some stuff into variables - host and basepath
+
+## Notes on Style
+
+I have included my routes in the `core.clj` file. This is fine when we are working on small, demo apps, but it is a good idea to separate these out when the app scales. For now though, it will all be easier to reason about if its in the same file.
+
+## Housekeeping
+
+- Install Clojure
+
+  See the [Clojure Install Guide](https://clojure.org/guides/getting_started)
+
+## Quick Start
+
+- Start the web server
+
+  ```bash
+  make serve
+  ```
+
+- Visit the terminal and start the reloaded workflow
+
+  ```bash
+  web-server.dev=> (go)
+  ```
+
+- Visit the app
+
+  http://localhost:3001/phonebook-api
+
+You should now see the swagger ui. The swagger ui should show you a route called `http://localhost:3001/phonebook-api/entry`. This route should have 4 methods associated to it: `GET`, `PUT`, `DELETE` and `POST`.
+
+## Learnings
+
+- [clojure.data](#clojure.data)
+- [Swagger](#swagger)
+- [Bidi](#bidi)
+
+### clojure.data
+
+If you're ever looking to convert clojure -> json checkout the `clojure.data.json` library. You can see it used in this app in `core.clj`
+
+### Swagger
+
+As you can see, we are using the `swaggered` function which is going to provide a built-in swagger-ui for our routes, however, there is an alternative to this where you can write your own custom setup for the swagger ui. This will be covered in future entries.
+
+### Bidi
+
+When working with bidi routes, be careful with the vectors if you are using the terse version. It is very easy to mess them up and kind of difficult to keep track of them when you are just starting out with it.
--- web-server-api/src/web_server/core.clj
+++ web-server-api/src/web_server/core.clj
@@ -1,26 +1,78 @@
 (ns web-server.core
   "Example of a basic yada web server"
   (:require
+    [clojure.data.json     :as json]
     [clojure.tools.logging :as log]
-    [integrant.core :as ig]
-    [yada.yada :as yada]))
+    [integrant.core        :as ig]
+    [yada.swagger          :as swagger :refer [swaggered]]
+    [yada.yada             :as yada]))
 
 
 ;; Web Server Routes
 ;; -----------------------------------------------------------------------------
 
 
-(defn route []
-  ["/hello" (yada/handler "Hello World!\n")])
+(defn phonebook-entry-route
+  "Resource describing what we should do when the user hits /phonebook/{id}"
+  []
+  (yada/resource
+    {:id          :web-server.core/phonebook-entry
+     :description "Phonebook entry"
+     :parameters  {:path {:id Long}}
+     :produces    [{:media-type #{"application/json;q=0.8"}
+                    :charset "UTF-8"}]
+     :methods
+     {:get
+      {:swagger/tags ["default" "getters"]
+       :response
+       (fn [ctx]
+         (json/write-str {:success true :type :get}))}
+
+      :put
+      {:swagger/tags ["default" "getters"]
+       :response
+       (fn [ctx]
+         (json/write-str {:success true :type :put}))}
+
+      :delete
+      {:swagger/tags ["default" "getters"]
+       :response
+       (fn [ctx]
+         (json/write-str {:success true :type :delete}))}
+
+      :post
+      {:swagger/tags ["default" "getters"]
+       :response
+       (fn [ctx]
+         (json/write-str {:success true :type :post}))}}}))
 
 
-;; Web Server Integrant Lifecycle Methods
+(defn swaggered-route
+  [routes]
+  (swaggered
+    routes
+    {:info     {:title       "Phonebook"
+                :version     "1.0"
+                :description "Phonebook routes"}
+     :host     "localhost:3001"
+     :schemes  ["http"]
+     :basePath "/phonebook-api"})) ;; TODO MAKE ME A VARIABLE
+
+
+(defn routes
+  []
+  (let [phonebook-entry-route* ["/entry" [[["/" :id] (phonebook-entry-route)]]]]
+    ["/"
+      [["phonebook-api" (swaggered-route phonebook-entry-route*)]]]))
+
+
+;; Web Server - Integrant Lifecycle Methods
 ;; -----------------------------------------------------------------------------
 
 
 (defmethod ig/init-key :web-server/core
   [_ {:web-server.core/keys [host port] :as config}]
-  (let [listener (yada/listener (route) {:port port})]
+  (let [listener (yada/listener (routes) {:port port})]
     (log/infof "Started http server on port %s" (:port listener))
     {:listener listener
      ;; host is used for announcement in dev
